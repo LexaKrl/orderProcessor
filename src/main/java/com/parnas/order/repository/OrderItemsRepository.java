6ea3bb20a8c205739d1515cdd416415693ccce1a
Alex Kirilin <kirilin.75.ru@gmail.com>
2026-06-12 21:27:51 +0300

    update order and orderItems, add repositories for these entities and add liquibase migration

---
+++ src/main/java/com/parnas/order/repository/OrderItemsRepository.java
@@ -0,0 +1,10 @@
+package com.parnas.order.repository;
+
+import com.parnas.order.model.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemsRepository extends JpaRepository<OrderItem, Long> {
+
+}
--- src/test/java/com/parnas/order/TestClass.java
+++
@@ -1,4 +0,0 @@
-package com.parnas.order;
-
-public class TestClass {
-}
